b39b0b5bb72fa3934e45f3fba9917eceb31c674b
Elaf <elal0001@stud.kea.dk>
2024-12-12 00:58:39 +0100

    nu kan du tilføj en rapport i databasen og på siden

--- src/main/java/com/example/bilabonnement/Repository/StatistikRepository.java
+++ src/main/java/com/example/bilabonnement/Repository/StatistikRepository.java
@@ -9,12 +9,13 @@ public class StatistikRepository {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    //finder hvor mange biler der er udlejde
 
     public int findAntalLejedeBiler() {
         String sql = "SELECT COUNT(*) FROM Lejeaftale WHERE CURDATE() BETWEEN start_dato AND slut_dato";
         return jdbcTemplate.queryForObject(sql, Integer.class);//kan godt bruge int istedet for wrapperklasser men den skal ikke retuner null
     }
-
+// viser sammlet pris på lejedebiler
     public double findSamletPrisForLejedeBiler() {
         String sql = "SELECT SUM(pris) FROM Lejeaftale WHERE CURDATE() BETWEEN start_dato AND slut_dato";
         return jdbcTemplate.queryForObject(sql, Double.class);//kan godt brug double som primativ typr
